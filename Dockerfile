ee52850261c818177a74dd835ed8871889bbb257
miyazaki <kota_miyazaki@adforce.co.jp>
2024-10-03 14:00:34 +0900

    feat: add build-layer.sh

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM public.ecr.aws/sam/build-python3.11:latest
 
 RUN yum -y update && yum -y install zip
 
+COPY build-layer.sh /work/build-layer.sh
+
 WORKDIR /work
---
+++ build-layer.sh
@@ -0,0 +1,11 @@
+# clear
+rm -f /work/layer.zip
+rm -rf /tmp/python
+
+# install
+mkdir -p /tmp/python
+pip install -r /work/requirements.txt -t /tmp/python
+
+# 
+cd /tmp
+zip -r /work/layer.zip ./python
